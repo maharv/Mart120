b27ade85a1b8aca2f1b0d5e12004306fd7ce7e3f
maharv <89863712+maharv@users.noreply.github.com>
2021-11-01 16:30:30 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(300, 500);
+}
+
+function draw() {
+  background(220);
+  ellipse(150,250,200,300);
+  triangle(140, 240, 145, 250, 170, 270)
+}
+
+function draw() {
+  background(240);
+  strokeWeight(4);
+  ellipse(150,250,200,300);
+  triangle(140, 240, 150, 220, 160, 240);
+  point(80,200);
+  point(200,200);
+  square(80,200,20);
+  square(200,200,20);
+  line(83,178,127,193);
+  line(168,193,215,178);
+  point(107,211);
+  point(189,211);
+  triangle(80,276,150,385,220,276);
+  textSize(32)
+  text('MARK', 10, 30);
+  //circle(mo395useX, mouseY, 25);
+}
